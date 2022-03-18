984512df1addd9dbad6c0203949074f1eecb2b94
Belle Duran <bduran04@gmail.com>
2022-03-17 21:18:28 -0500

    toast for registration successful

--- pages/register.js
+++ pages/register.js
@@ -1,24 +1,25 @@
-import {Card, Row, Input, Button, Grid, Link} from "@geist-ui/react";
+import {Card, Row, Input, Button, Grid, useToasts} from "@geist-ui/react";
 import { useRouter } from 'next/router'
 import {supabase} from "../utils/supabaseClient";
 import {useState} from "react"
 
-//create a toast that notifies when a User has successfully registered to the database
-
 const Register = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+
     const router = useRouter();
 
+    const [, setToast ] = useToasts()
+    const click = () => setToast({ text: 'Registration Successful!', delay: 2000, type: "success" })
+
     const register = async () => {
-        console.log(username, password)
         const { data, error } = await supabase
             .from('Users')
             .insert([
                 { username, password },
-            ]); if (data.status === 200) {
-            window.alert("registration successful");
-           await router.push('/')
+            ]); if (!error) {
+            click();
+           await router.push('/login')
         }
     }
 
@@ -27,7 +28,7 @@ const Register = () => {
             <Grid gap={1.5} xs={12} md={6}>
                 <Card shadow style={{margin: '3rem', padding: '1rem'}}>
                     <Row style={{marginBottom: '1rem'}}> <Input onChange={(e) => setUsername(e.target.value)} placeholder="Username" width="100%" /> </Row>
-                    <Row style={{marginBottom: '1rem'}}> <Input onChange={(e) => setPassword(e.target.value)} placeholder="Password" width="100%"/></Row>
+                    <Row style={{marginBottom: '1rem'}}> <Input.Password onChange={(e) => setPassword(e.target.value)} placeholder="Password" width="100%"/></Row>
                     <Button type="success" width="0.5rem" mx="1rem" style={{marginBottom: '1rem'}} onClick={async () => await register()}>Register</Button>
                 </Card>
             </Grid>
